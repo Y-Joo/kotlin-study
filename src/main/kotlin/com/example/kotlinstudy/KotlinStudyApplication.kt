b281c17d99171c12e2045ac90cde7cc1de1bd4aa
Y-Joo <wldudwn99@gmail.com>
2023-11-08 18:23:03 +0900

    Feat: Added db connection service practice

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -4,9 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @SpringBootApplication
 class KotlinStudyApplication
@@ -16,16 +15,34 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageController {
+class MessageController(val messageService: MessageService) {
     @GetMapping("/")
-    fun index() = listOf(
-        Message("test1", "test1"),
-        Message("test2", "test2"),
-        Message("test3", "test3"),
-    )
+    fun index(): List<Message> = messageService.findMessage()
+    @GetMapping("/{id}")
+    fun index(@PathVariable id: String): List<Message> = messageService.findMessageById(id)
+    @PostMapping("/")
+    fun post(@RequestBody message: Message){
+        messageService.save(message)
+    }
 }
 
 data class Message(val id: String?, val text: String)
 
+@Service
+class MessageService(val db: JdbcTemplate){
+    fun findMessage(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = '$id'") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun save(message: Message){
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update("insert into messages values (?, ?)",
+        id, message.text
+        )
+    }
 }
 
