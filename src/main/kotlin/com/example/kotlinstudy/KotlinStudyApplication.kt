b6d0917c8dcd5e817b739db713eea2dd3fffdb27
Y-Joo <wldudwn99@gmail.com>
2023-11-08 17:39:10 +0900

    Feat: Added data class practice

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -2,6 +2,8 @@ package com.example.kotlinstudy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -16,5 +18,14 @@ fun main(args: Array<String>) {
 @RestController
 class MessageController {
     @GetMapping("/")
-    fun index(@RequestParam("name") name:String) = "Hello, $name!"
+    fun index() = listOf(
+        Message("test1", "test1"),
+        Message("test2", "test2"),
+        Message("test3", "test3"),
+    )
 }
+
+data class Message(val id: String?, val text: String)
+
+}
+
